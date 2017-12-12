aef3a835aa8345360bfe18d66a77cd99dcc6e0eb
pereiraJr <lielsonjr@hotmail.com>
2017-12-12 08:29:08 -0300

    WEBHOOK

--- VersionamentoGit/VersionamentoGit/Program.cs
+++ VersionamentoGit/VersionamentoGit/Program.cs
@@ -11,7 +11,8 @@ namespace VersionamentoGit
         static void Main(string[] args)
         {
             Console.WriteLine("Continuous Integration Succes");
-         //   Console.WriteLine("Test Web Hook GITHUB");
+            Console.WriteLine("Test Web Hook GITHUB");
+            Console.WriteLine("Test Web Hook GITHUB");
         }
     }
 }
