0e340724f2021aa590db8a65d62f2711723a047b
pereiraJr <lielsonjr@hotmail.com>
2017-12-11 21:06:23 -0300

    Initial Commit

---
+++ VersionamentoGit/VersionamentoGit/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersionamentoGit
+{
+    public static class Class1
+    {
+       public static int soma(int param1, int param2)
+        {
+            return param1 + param2;
+        }
+
+        public static int subtracao(int param1, int param2)
+        {
+            return param1 - param2;
+        }
+    }
+}
